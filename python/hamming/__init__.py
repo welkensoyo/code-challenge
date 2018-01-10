08718dc6da99997e0bfe87e249f60deb6d8070c1
Derek Bartron <eatmeimadanish@gmail.com>
2018-01-10 15:07:02 -0600

    Upload __init__.py
    Upload final python submission.

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,13 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(dna_string_1, dna_string_2):
+    #support Python 2.7 or Python 3.x
+    try:
+        xrange
+    except NameError:
+        xrange = range
+    if len(dna_string_1) != len(dna_string_2):
+        raise ValueError('DNA strands must be of equal length.')
+    #return list of indexes that do not match, then use length to get total.
+    return len([index for index in xrange(len(dna_string_1)) if dna_string_1[index] != dna_string_2[index]])
